a58d7b199eeb71b01b334debe06991f74b17f345
anjali790 <anjaliruby790@gmail.com>
2022-10-08 16:15:18 +0530

    Login Form

---
+++ loginform/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Login } from './Components/Login';
+import { LoginForm } from './Components/LoginForm';
+
+function App() {
+  return (
+    <>
+      <LoginForm />
+      <Login />
+    </>
+  );
+}
+
+export default App;
